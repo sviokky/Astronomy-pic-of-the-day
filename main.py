63d4c451fc4b46291373f82b3dc8e7bf5a0b34d4
Zviad Kakauridze <zviadd97@gmail.com>
2026-05-17 18:37:03 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import requests
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: transparent;
+    }
+
+    /* Background layer */
+    .bg {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url("https://images.unsplash.com/photo-1446776811953-b23d57bd21aa");
+        background-size: cover;
+        background-position: center;
+        filter: blur(12px);
+        transform: scale(1.1);
+        z-index: -1;
+    }
+    </style>
+
+    <div class="bg"></div>
+    """,
+    unsafe_allow_html=True
+)
+
+api_key = "2QtJ2e56PrhcQB5fSZhZC7xebRMXH7zPIeu27PCy"
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+
+response = requests.get(url)
+data = response.json()
+
+st.title("Astronomy Picture of the Day")
+st.write("This webpage shows the picture of the day by NASA every day")
+st.write(data["title"])
+st.image(data["url"])
+st.write(data["explanation"])
