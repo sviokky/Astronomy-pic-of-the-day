d6988069d2ad7783ff07fdf065beb6ac068f84c2
Zviad Kakauridze <zviadd97@gmail.com>
2026-05-17 18:45:09 +0400

    3rd commit

--- main.py
+++ main.py
@@ -35,6 +35,13 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 response = requests.get(url)
 data = response.json()
 
+
+st.set_page_config(
+    page_title="APOD",
+    page_icon="⭐",
+    layout="centered"
+)
+
 st.title("Astronomy Picture of the Day")
 st.write("This webpage shows the picture of the day by NASA every day")
 st.write(data["title"])
