e67ae16c54fd5eda3dec65d7f5ef4cebbd33a3de
Zviad Kakauridze <zviadd97@gmail.com>
2026-05-17 19:03:52 +0400

    polishing...

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import streamlit as st
 import requests
 
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background: transparent;
-    }
-
-    /* Background layer */
-    .bg {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url("https://images.unsplash.com/photo-1446776811953-b23d57bd21aa");
-        background-size: cover;
-        background-position: center;
-        filter: blur(12px);
-        transform: scale(1.1);
-        z-index: -1;
-    }
-    </style>
-
-    <div class="bg"></div>
-    """,
-    unsafe_allow_html=True
-)
-
 api_key = "2QtJ2e56PrhcQB5fSZhZC7xebRMXH7zPIeu27PCy"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
 response = requests.get(url)
 data = response.json()
 
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background: transparent;
+    }}
+
+    .bg {{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url("{data["url"]}");
+        background-size: cover;
+        background-position: center;
+        filter: blur(15px);
+        transform: scale(1.1);
+        z-index: -1;
+    }}
+    </style>
+
+    <div class="bg"></div>
+    """,
+    unsafe_allow_html=True
+)
 
 st.set_page_config(
     page_title="APOD",
